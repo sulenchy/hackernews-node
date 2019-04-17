aa8ef80112c3265a111c09bd1f08eba27bb884f0
sulenchy <sulaimanabiodun172@gmail.com>
2019-04-17 13:11:17 +0100

    simple graphql query

--- src/index.js
+++ src/index.js
@@ -3,18 +3,37 @@ const { GraphQLServer } = require('graphql-yoga')
 // 1
 const typeDefs = `
 type Query {
-  info: String!,
-  me: String!
-}
+    info: String!
+    feed: [Link!]!
+  }
+  
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
 `
 
-// 2
-const resolvers = {
-  Query: {
-    info: () => "null",
-    me:() => `My name is sulenchy`
+// 1
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  
+  const resolvers = {
+    Query: {
+      info: () => `This is the API of a Hackernews Clone`,
+      // 2
+      feed: () => links,
+    },
+    // 3
+    Link: {
+      id: (parent) => parent.id,
+      description: (parent) => parent.description,
+      url: (parent) => parent.url,
+    }
   }
-}
 
 // 3
 const server = new GraphQLServer({
